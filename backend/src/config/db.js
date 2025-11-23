815ba928c353941a2c673e9978c46633a5153907
Thái Văn Phúc <phuc100662@donga.edu.vn>
2025-11-23 17:58:49 +0700

    up

---
+++ backend/src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("");
+    console.log("MongoDB Connected....", conn.connection.host);
+  } catch (error) {
+    console.error("MongoDB Connection fail...", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
