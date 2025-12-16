a9af9e1accdf0f4a0a532889eb1216f88789870b
tanhoang11 <hoang100333@donga.edu.vn>
2025-12-16 20:25:56 +0700

    fix auth

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
+router.post("/register", authController.register);
+
+router.post("/login", authController.login);
+
+// Test role
+router.get(
+  "/admin",
+  authMiddleware,
+  roleMiddleware(["ADMIN"]),
+  (req, res) => {
+    res.json({ message: "Welcome admin!" });
+  }
+);
+
+module.exports = router;
