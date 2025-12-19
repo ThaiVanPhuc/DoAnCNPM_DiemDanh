272465f7fa8b757e777f2a79635eda7f93b2c8a2
Hong Phuc Nguyen <nguyenhongphucms02@gmail.com>
2025-12-19 13:15:24 +0700

    update

--- frontend-react/src/routes/AppRoutes.jsx
+++ frontend-react/src/routes/AppRoutes.jsx
@@ -6,9 +6,12 @@ import PrivateRoute from "./PrivateRoute"; // ✔ chỉ import 1 lần
 import AdminDashboard from "../pages/AdminDashboard";
 import MyProfilePage from "../pages/MyProfilePage";
 import FacePage from "../pages//FacePage";
+import FaceTrainingApp from "../pages/FaceAttendanceApp";
+import FaceAttendanceApp from "../pages/FaceTrainingApp";
 function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
 
@@ -20,7 +23,24 @@ function AppRoutes() {
           </PrivateRoute>
         }
       />
+{/* Face Recognition - BẮT BUỘC LOGIN */}
+      <Route
+        path="/training"
+        element={
+          <PrivateRoute>
+            <FaceTrainingApp />
+          </PrivateRoute>
+        }
+      />
 
+      <Route
+        path="/attendance"
+        element={
+          <PrivateRoute>
+            <FaceAttendanceApp />
+          </PrivateRoute>
+        }
+      />
       <Route
         path="/admin/dashboard"
         element={
